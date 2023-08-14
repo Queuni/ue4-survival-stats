4424999000d2d109296c17c4c168e365516978e2
Queuni <Queuni@users.noreply.github.com>
2023-08-14 20:15:39 +0000

    Adjust buffer size for the stream reader to reduce memory usage

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,5 @@
 # Implement proper cleanup of resources when the process receives SIGTERM
 
 # Simplify the main loop by extracting request handling into a dedicated function
+
+# Update README with installation steps and environment variable documentation
