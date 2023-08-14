ddbf2598fd5d1c8acd14e20c62c0a3bee1fe4962
Queuni <Queuni@users.noreply.github.com>
2023-08-14 14:34:53 +0000

    Improve error message when the required env var is not set

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,3 +6,5 @@
 # Improve performance by caching the result of the expensive lookup
 
 # Clean up the formatting and run the linter on the changed files
+
+# Improve error message when the required env var is not set
--- src/config.py
+++ src/config.py
@@ -4,3 +4,5 @@
 # Add a note in the README about the breaking change in 2.0
 
 # Improve the default config so it works out of the box for dev
+
+# Adjust timeout and retry settings based on production observations
